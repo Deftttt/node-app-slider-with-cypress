c8f5c1279f0b239b6f65aefc21d8e22ab893605d
Piotr Stasicki <piotrstasicki@gmail.com>
2025-03-26 21:22:23 +0100

    update

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -16,3 +16,37 @@ describe('Swiper Gallery Test', function () {
     cy.get('.swiper-slide-active').should('contain', 'Paris');
   });
 });
+
+describe('Swiper Gallery Test #2', function () {
+  it('Check whether the description of each slide is displayed correctly.', function () {
+    cy.visit('http://localhost:3000');
+
+    cy.get('.swiper-slide-active .card-description').should('exist');
+    cy.get('.swiper-slide-active .card-description h1')
+      .should('exist')
+      .and('have.text', 'Rome');
+    cy.get('.swiper-slide-active .card-description p')
+      .should('exist')
+      .and('have.text', 'Italy');
+
+    cy.get('.swiper-button-next').click();
+    cy.wait(2000);
+    cy.get('.swiper-slide-active .card-description').should('exist');
+    cy.get('.swiper-slide-active .card-description h1')
+      .should('exist')
+      .and('have.text', 'London');
+    cy.get('.swiper-slide-active .card-description p')
+      .should('exist')
+      .and('have.text', 'United Kingdom');
+
+    cy.get('.swiper-button-next').click({ force: true });
+    cy.wait(2000);
+    cy.get('.swiper-slide-active .card-description').should('exist');
+    cy.get('.swiper-slide-active .card-description h1')
+      .should('exist')
+      .and('have.text', 'Paris');
+    cy.get('.swiper-slide-active .card-description p')
+      .should('exist')
+      .and('have.text', 'France');
+  });
+});
